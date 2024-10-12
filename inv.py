3165a58468cbfd15b7d315f5bc03010c9b36c212
keetane <yu.takahashi.k@gmail.com>
2024-10-13 06:28:48 +0900

    invert_coordinates was edited

--- inv.py
+++ inv.py
@@ -1,11 +1,11 @@
 from pymol import cmd
 
-def invert_coordinates():
-    model = cmd.get_model('visible')
+def invert_coordinates(selection='visible'):
+    model = cmd.get_model(selection)
     for atom in model.atom:
         atom.coord = [-atom.coord[0], -atom.coord[1], -atom.coord[2]]
     cmd.load_model(model, 'inv')
     cmd.hide('lines', 'inv')
     cmd.show('cartoon', 'inv')
 
-cmd.extend('invert_coordinates', invert_coordinates)
+cmd.extend('invertcoordinates', invert_coordinates)
