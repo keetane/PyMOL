3139fc638dc84e77ec66efa1e42f07816505ee83
keetane <yu.takahashi.k@gmail.com>
2024-10-20 23:21:29 +0900

    README.md was created

---
+++ README.md
@@ -0,0 +1,65 @@
+# PyMOL Plugin
+This plugin is based on PyMOL, which has been developed as OSS, with respect and thanks to the PyMOL wiki and the voluntary contributors who have made it freely available.  
+The basic development environment is Mac M1 or later, but these Plusins have confirmed that it also works on windows.
+## PyMOL install
+For easy install of PyMOL, I recommend to use `conda`.  
+If you already installed Anaconda or miniconda, just install with command in terminal as follow,
+>conda create -n your-env -y -c conda-forge python=3.9 pymol-open-source rdkit
+
+
+## PyMOL instructions
+For basic information, I will recommend the link below,
+1. University of Osaka
+2. PyMOL book
+    https://yoshitakamo.github.io/pymol-book/  
+    The owere of this tutorial, Dr. Yoshitaka Moriwaki, is one of great contributor of PyMOL. His support helped me enormously with my start of PyMOL. I appreciate his support and contribution to OSS community.
+3. PyMOL wiki
+    https://pymolwiki.org/index.php/Main_Page
+
+## PyMOL Plugin
+This Plugins were described by Python, used pymol and rdkit.
+### residues.py
+Visualisation is a very important element in OSS-SBDD, and while PyMOL has a rich implementation of commands for visualisation, this plugin aims to make it easier. Let's get started.
+
+When developing kinase inhibitors, it is necessary to find gatekeepers located deep in the ATP pocket, e.g. threonine, methionine and lysine.  
+Or, it will be necessary to identify from the structural information whether the lysine is in the right position on the surface of your target for which you want to explore a degrader.  
+One-letter command for visualization of respective amino acid residues helps you to find the all residues you want to know.  
+Type One-letter residue and press Enter, then the sticks of residues you want to know will be showed for enabled objects.
+
+
+
+
+Annoying water molecules? Type `del`. You would like to show them again? Type `sol`. Don't you need the hydrophobic hydrogens? Type `ph`. You can focus more into proteins or nucleic acid oligomers.
+
+
+
+Lots of PDB files includes the structural information of not only monomer, but also multiple complexes. Type `cc` to identify the respective chain information by coloring.  
+I like sipmple color cordinates. `white` command will make the color of cartoon and carbon chains white.  
+If you would like to change the cartoon color by secondary structural information, type `dssp` to change the color of helixes into red, sheets into yellow and loops into gray, respectively.  
+AF2 predicted structure? Type `plddt` to turned cartoon's color by plddt scores.  
+
+Some PDB files includes the ligand infomation, which bounds to the receptor molecule to interact and get some functions.  
+This grateful and useful information will help you to design or modify the molecules. Let's find the intaraction information with this plugin.
+`byres` command will make objects of ligand and residues within 4A distance, then navigate you there. Type `tag` to show the residue's name selected. Polar contact object is also created by `byres` command.
+Not organid ligand? Well, you may see the peptide or nucleic acid ligand. Typing `at` after selecting the ligand to help you find the polar contacts you would like to see.  
+Would you like to see the surface around the ligand? Type `well` to show the surface of around the ligand within 5A distance.
+
+
+
+
+
+## PyMOL Plugins from other sources
+My favarite Plugins were described below. Great thanks to all of contributors for PyMOL
+
+### how to install Plugins
+1. install as addin from PyMOL  
+
+
+2. load Plugins from source
+
+3. load Plugins from local file
+
+
+
+### Color By Mutations
+https://pymolwiki.org/index.php/Color_By_Mutations
--- residues.py
+++ residues.py
@@ -1,17 +1,16 @@
 from pymol import cmd
-
-# One-letter Labeling
+# Visualization
+### One-letter Labeling
 one_letter ={'VAL':'V', 'ILE':'I', 'LEU':'L', 'GLU':'E', 'GLN':'Q', \
 'ASP':'D', 'ASN':'N', 'HIS':'H', 'TRP':'W', 'PHE':'F', 'TYR':'Y',    \
 'ARG':'R', 'LYS':'K', 'SER':'S', 'THR':'T', 'MET':'M', 'ALA':'A',    \
 'GLY':'G', 'PRO':'P', 'CYS':'C'}
 
-
 cmd.alias('tag', 'label n. CA and sele, "%s%s" % (one_letter[resn], resi)')
 cmd.alias('b', 'label n. CA and sele, b')
 cmd.alias('untag', 'hide label, sele')
 
-# hide sticks, show residues
+### hide sticks, show residues
 cmd.alias('k', 'hide sticks, polymer.protein; show sticks, enabled and resn lys')
 cmd.alias('c', 'hide sticks, polymer.protein; show sticks, enabled and resn cys')
 cmd.alias('s', 'hide sticks, polymer.protein; show sticks, enabled and resn ser')
@@ -35,82 +34,13 @@ cmd.alias('v', 'hide sticks, polymer.protein; show sticks, enabled and resn val'
 cmd.alias('me', 'hide sticks, polymer.protein; show sticks, enabled and resn val; show sticks, enabled and resn leu; show sticks, enabled and resn ile; show sticks, enabled and resn met')
 cmd.alias('z', 'hide sticks, polymer.protein')
 
-# water and hydrophobic hydrogens manupilations
+### water and hydrophobic hydrogens manupilations
 cmd.alias('del', 'as nb_spheres, solvent; hide nb_spheres')
 cmd.alias('sol', 'as nb_spheres, solvent')
 cmd.alias('ph', 'hide (hydro) and enabled and (elem C extend 1)')
 
-## interaction analysis
-# zoom into around ligand
-def see(distance=8):
-    cmd.zoom('byres organic around %s and enabled' % distance)
-cmd.extend('see', see)
 
-# show lines around ligand
-def line(distance=4):
-    cmd.show('lines', 'byres organic around %s and enabled' % distance)
-cmd.extend('line', line)
-
-# select residues with distance around ligand
-def grab(sele='organic', distance=4):
-    cmd.select('sele', 'byres %s around %s and enabled' % (sele, distance))
-    cmd.enable('sele')
-cmd.extend('grab', grab)
-
-# show polar contact of ligand to residues
-def hb():
-    cmd.select('ligand', 'organic and enabled')
-    cmd.dist('contact', '(ligand)', '(byobj (ligand)) and (not(ligand))', quiet=1, mode=2, label=0, reset=1)
-    cmd.enable('contact')
-cmd.extend('hb',hb)
-
-def dock(ligand='enabled and organic'):
-    cmd.delete('polar_conts')
-    cmd.dist("polar_conts","%s" % ligand, "(not %s)" % ligand ,quiet=1,mode=2,label=0,reset=1)
-    cmd.enable("polar_conts")
-cmd.extend('dock', dock)
-
-# show polar contact of sele to residues
-def at():
-    cmd.dist('contact', '(sele)', '(byobj (sele)) and (not(sele))', quiet=1, mode=2, label=0, reset=1)
-    cmd.enable('contact')
-    see(distance)
-cmd.extend('at',at)
-
-# create object of ligand and residues with distance
-def byres(distance=8):
-    object_name = 'byres' + str(distance)
-    cmd.create('Lig', 'enabled and organic')
-    cmd.create(object_name, 'byres organic around %s and enabled' % distance)
-    cmd.disable('!' + object_name)
-    cmd.enable('Lig')
-    line()
-    dock('Lig')
-    see()
-    grab()
-cmd.extend('byres',byres)
-
-# show surface of residues with distance from ligand
-def well(distance=5):
-    cmd.show('surface', 'byres organic expand %s and enabled' % distance)
-cmd.extend('well',well)
-
-# change surface cavity mode
-def cav():
-    cmd.set('surface_cavity_mode', 2)
-cmd.extend('cav',cav)
-
-def wall():
-    cmd.set('surface_cavity_mode', 0)
-cmd.extend('wall',wall)
-
-# preset ligand cartoon
-def pl():
-    preset.ligand_cartoon("enabled", _self=cmd)
-    cmd.show('spheres', 'solvent')
-cmd.extend('pl',pl)
-
-## coloring
+### coloring
 def white():
     util.cba(144, "enabled", _self=cmd)
 cmd.extend('white',white)
@@ -138,13 +68,97 @@ def plddt():
     cmd.color('white', 'enabled and sc. and elem C')
 cmd.extend('plddt',plddt)
 
+## interaction analysis
+### zoom into around ligand
+def see(distance=8):
+    cmd.zoom('byres organic around %s and enabled' % distance)
+cmd.extend('see', see)
+
+### show lines around ligand
+def line(distance=4):
+    cmd.show('lines', 'byres organic around %s and enabled' % distance)
+cmd.extend('line', line)
+
+### select residues with distance around ligand
+def grab(sele='organic', distance=4):
+    cmd.select('sele', 'byres %s around %s and enabled' % (sele, distance))
+    cmd.enable('sele')
+cmd.extend('grab', grab)
+
+### show polar contact of ligand to residues in the enabled same object
+def hb():
+    cmd.select('ligand', 'organic and enabled')
+    cmd.dist('contact', '(ligand)', '(byobj (ligand)) and (not(ligand))', quiet=1, mode=2, label=0, reset=1)
+    cmd.enable('contact')
+cmd.extend('hb',hb)
+
+### show polar contact of ligand to residues between the enabled different object
+def dock(ligand='enabled and organic'):
+    cmd.delete('polar_conts')
+    cmd.dist("polar_conts","%s" % ligand, "(not %s)" % ligand ,quiet=1,mode=2,label=0,reset=1)
+    cmd.enable("polar_conts")
+cmd.extend('dock', dock)
+
+### show polar contact of sele to residues
+def at():
+    cmd.dist('contact', '(sele)', '(byobj (sele)) and (not(sele))', quiet=1, mode=2, label=0, reset=1)
+    cmd.enable('contact')
+    see(distance)
+cmd.extend('at',at)
+
+### create object of ligand and residues with distance
+def byres(distance=8, ligand='organic and enabled'):
+    object_name = 'byres' + str(distance)
+    cmd.create('Lig', '%s' % ligand)
+    cmd.create(object_name, 'byres %s around %s' % (ligand, distance))
+    cmd.disable('!' + object_name)
+    cmd.enable('Lig')
+    line()
+    dock('Lig')
+    see()
+    grab()
+cmd.extend('byres',byres)
+
+# def byres(distance=8):
+#     object_name = 'byres' + str(distance)
+#     cmd.create('Lig', 'enabled and organic')
+#     cmd.create(object_name, 'byres organic around %s and enabled' % distance)
+#     cmd.disable('!' + object_name)
+#     cmd.enable('Lig')
+#     line()
+#     dock('Lig')
+#     see()
+#     grab()
+# cmd.extend('byres',byres)
+
+### show surface of residues with distance from ligand
+def well(distance=5):
+    cmd.show('surface', 'byres organic expand %s and enabled' % distance)
+cmd.extend('well',well)
+
+### change surface cavity mode
+def cav():
+    cmd.set('surface_cavity_mode', 2)
+cmd.extend('cav',cav)
+
+def wall():
+    cmd.set('surface_cavity_mode', 0)
+cmd.extend('wall',wall)
+
+### preset ligand cartoon
+def pl():
+    preset.ligand_cartoon("enabled", _self=cmd)
+    cmd.show('spheres', 'solvent')
+cmd.extend('pl',pl)
+
+
 ## sequencing
-# print fasta sequence of enabled object
+### print fasta sequence of enabled object
 def fasta():
     print(cmd.get_fastastr('enabled'))
 cmd.extend('fasta',fasta)
 
-# print fasta sequence of sele
+### print fasta sequence of sele object
 def seq():
     print(cmd.get_fastastr('sele'))
 cmd.extend('seq',seq)
