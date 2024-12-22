01b54b9274453335ea4195dda476739c5a2a4879
keetane <yu.takahashi.k@gmail.com>
2024-12-23 05:33:09 +0900

    grid.py was updated

--- grid.py
+++ grid.py
@@ -186,4 +186,46 @@ def smina_score(docked='docked_.sdf'):
 cmd.extend('smina_score', smina_score)
 
 
+def Smina(ligand='enabled and organic', receptor='enabled and polymer.protein', grid=None, padding=4.0, n_pose=3, seed=0, comment=None):
+    # temporary file of receptor
+    rec='/tmp/rec.pdb'
+    # save pdb file of protein from enabled object
+    cmd.save(rec, receptor)
+
+    # temporary file of ligand
+    lig='/tmp/lig.pdb'
+    # save pdb file of protein from enabled object
+    cmd.save(lig, ligand)
+    if grid==None:
+        grid=ligand
+    else:
+        grid = grid + ' and polymer'    
+
+    # defining grid box
+    ([minX, minY, minZ], [maxX, maxY, maxZ]) = cmd.get_extent(grid)
+    center_of_mass = [(minX + maxX) / 2, (minY + maxY) / 2, (minZ + maxZ) / 2]
+    minX = minX - float(padding)
+    minY = minY - float(padding)
+    minZ = minZ - float(padding)
+    maxX = maxX + float(padding)
+    maxY = maxY + float(padding)
+    maxZ = maxZ + float(padding)
+
+    size_x = maxX - minX
+    size_y = maxY - minY
+    size_z = maxZ - minZ
+
+    center_x = (minX + maxX) / 2
+    center_y = (minY + maxY) / 2
+    center_z = (minZ + maxZ) / 2
+
+    if comment==None:
+        comment=''
+    print("\nExcuting Smina")
     
+    # docking = f'smina -r {rec} -l {lig} -o ./docked_{comment}.sdf --center_x {center_x} --center_y {center_y} --center_z {center_z} --size_x {size_x} --size_y {size_y} --size_z {size_z} --seed {seed} --num_modes {n_pose}'
+    docking = f'smina -r {rec} -l {lig} -o ./docked_{comment}.sdf --center_x {center_x} --center_y {center_y} --center_z {center_z} --size_x {size_x} --size_y {size_y} --size_z {size_z} --seed {seed} --num_modes {n_pose}'
+    print(docking)
+    subprocess.call(docking.split(' '))    
+    cmd.load(f'./docked_{comment}.sdf')
+cmd.extend("Smina", Smina)
